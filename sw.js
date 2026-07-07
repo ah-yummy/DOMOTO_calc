fb418bc926391ab5302102231d185206edef41c7
ah-yummy <ahyummy57@gmail.com>
2026-07-08 00:47:14 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
-self.addEventListener("fetch", (e) => {});
+const CACHE_NAME = "domoto-calc-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./DOMOTOfavicon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
